2be018c7c78205525db7d5dc1a6a0a8b49cddc01
quanbao2603 <nguyenquanbao2632005@gmail.com>
2025-09-17 02:14:42 +0700

    update structure

---
+++ src/main/java/com/womtech/WomTechApplication.java
@@ -0,0 +1,12 @@
+package com.womtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WomTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WomTechApplication.class, args);
+	}
+}
