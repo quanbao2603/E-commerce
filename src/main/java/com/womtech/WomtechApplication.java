897d34604ae23bf79e1c687a7dc3f0ed9fb72f99
quanbao2603 <nguyenquanbao2632005@gmail.com>
2025-10-07 21:32:53 +0700

    delete demo application

--- src/main/java/com/womtech/WomtechApplication.java
+++ src/main/java/com/womtech/WomtechApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.womtech.repository")
-@EntityScan("com.womtech.entity")
 public class WomtechApplication {
     public static void main(String[] args) {
         SpringApplication.run(WomtechApplication.class, args);
