cbae7da7098d65ae98941ba1e3eb809dcd17abc5
quanbao2603 <nguyenquanbao2632005@gmail.com>
2025-10-13 12:25:51 +0700

    websocket demo still have error

--- src/main/java/com/womtech/repository/ChatRepository.java
+++ src/main/java/com/womtech/repository/ChatRepository.java
@@ -1,10 +1,16 @@
 package com.womtech.repository;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.womtech.entity.Chat;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, String> {
-	
+	List<Chat> findByUser_UserIDOrderByCreateAtDesc(String userID);
+
+    List<Chat> findBySupport_UserIDOrderByCreateAtDesc(String supportID);
+
+    Optional<Chat> findByUser_UserIDAndSupport_UserID(String userID, String supportID);
 }
