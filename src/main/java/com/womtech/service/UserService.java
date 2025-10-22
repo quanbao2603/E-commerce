6d2530e58e409867a5921330d30e9c9eb2126f32
tainguyen307 <23110307@student.hcmute.edu.vn>
2025-10-22 13:11:50 +0700

    add vnpay api

--- src/main/java/com/womtech/service/UserService.java
+++ src/main/java/com/womtech/service/UserService.java
@@ -25,7 +25,7 @@ public interface UserService extends BaseService<User, String>{
 
     LoginResponse login(LoginRequest request);
     
- // ✅ Thêm mới: trả về danh sách role name (viết hoa) theo userId
+ // Thêm mới: trả về danh sách role name (viết hoa) theo userId
     List<String> getRolesByUserId(String userId);
 
     // (tuỳ chọn) nếu cần dùng theo username sau này
